759749415bf0f8101894ff06259debe85763d9e0
Josué <josue.ger.torres.gar@gmail.com>
2024-08-22 16:42:12 -0600

    Moved input related functions from main to reader.rs

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ mod interpreter;
 mod scanner;
 mod parser;
 mod token;
-use token::Token;
 mod errors;
 mod grammar;
-
-use std::{collections::{HashMap, HashSet}, io::{self, BufRead, Write}};
+mod reader;
 
 fn welcome() {
     let mut message: String = String::new();
@@ -24,64 +22,10 @@ fn welcome() {
     println!("{}", message);
 }
 
-fn read_expression() -> String {
-    let mut expression = String::new();
-    let stdin = io::stdin();
-    stdin.lock().read_line(&mut expression).expect("Failed to read line. Restart the program and try again");
-    String::from(expression.trim())
-}
-
-fn read_bool(c: char) -> bool {
-    let mut input: String;
-    let stdin = io::stdin();
-    let mut boolean: Result<bool, _>;
-
-    loop {
-        print!("Enter value for {}: ", c);
-        io::stdout().flush().expect("Failed to flush stdout. Restart the program and try again");
-
-        input = String::new();
-        stdin.lock().read_line(&mut input).expect("Failed to read line. Restart the program and try again");
-
-        input = input.trim().parse().expect("");
-        if input.eq("0") {
-            return false
-        }
-        if input.eq("1") {
-            return true
-        }
-
-        boolean = input.parse();
-        if let Ok(b) = boolean {
-            return b
-        }
-
-        println!("Value is invalid. Please enter either a 0 or a 1")
-    }
-}
-
-fn read_simples(simples: Vec<char>) -> HashMap<Token, bool> {
-    let mut seen: HashSet<char> = HashSet::new();
-    let mut message = String::from("Now write the values of your simple propositions.\n");
-    message += "Write a 0 for \"false\" and a 1 for \"true\". You can also just write \"false\" and \"true\":\n";
-    println!("{}", message);
-
-    let mut values: HashMap<Token, bool> = HashMap::new();
-
-    for s in simples {
-        if let Some(_) = seen.get(&s)  { continue }
-
-        values.insert(Token::Sentence(s), read_bool(s));
-        seen.insert(s);
-    }
-
-    values
-}
-
 fn main() {
     welcome();
 
-    let proposition = read_expression();
+    let proposition = reader::read_expression();
     if proposition.is_empty() {
         println!("Proposition is empty.");
         return
@@ -103,7 +47,7 @@ fn main() {
     }
     println!();
 
-    let values = read_simples(simples);
+    let values = reader::read_simples(simples);
     println!();
 
     let eval = interpreter::interpret(values, res.unwrap());
