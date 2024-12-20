42ee37b553add3d2e74d5ad1492d52f9f32d79ba
Josué Torres <josue.ger.torres.gar@gmail.com>
2024-12-19 23:49:15 -0600

    Changed to simplifier removing useless struct

--- src/simplifier.rs
+++ src/simplifier.rs
@@ -1,69 +1,55 @@
 use crate::grammar::Expr;
 use crate::token::Token;
 
-pub fn simplify(expression: Expr) -> Expr {
-    let mut simplifier = Simplifier::new();
-    simplifier.simplify(expression)
-}
-
-struct Simplifier {
-    simplified: Expr,
-}
-
-impl Simplifier {
-    fn new() -> Self {
-        Simplifier {
-            simplified: Expr::null(),
-        }
-    }
     
-    fn simplify(&mut self, expression: Expr) -> Expr {
-        self.absorbtion(expression);
-        self.simplified.clone()
-    }
+pub fn simplify(expression: Expr) -> Expr {
+    let simplified = absorbtion(expression);
+    simplified
+}
 
-    fn absorbtion(&mut self, expression: Expr) {
-        self.simplified = expression.clone();
-        match expression {
-            // p | (!p & q) => p | q
-            Expr::Binary(left, operator, r) => {
-                let right: Expr;
-                let mut unary = false;
-                if let Expr::Grouping(e) = *r {
-                    right = *e;
-                } else {
-                    right = *r;
-                }
+fn absorbtion(expression: Expr) -> Expr {
+    let original = expression.clone();  // Must avoid cloning the value in future implementations
+    match expression {
+        // p | (!p & q) => p | q
+        Expr::Binary(left, operator, r) => {
+            let right: Expr;
+            let mut unary = false;
+            if let Expr::Grouping(e) = *r {
+                right = *e;
+            } else {
+                right = *r;
+            }
+            
+            // p      |          (!p & q)
+            // left   operator   right
+            if let Expr::Binary(mut leftr, operatorr, rightr) = right {
+                // !p      &           q
+                // leftr   operatorr   rightr
                 
-                // p      |          (!p & q)
-                // left   operator   right
-                if let Expr::Binary(mut leftr, operatorr, rightr) = right {
-                    // !p      &           q
-                    // leftr   operatorr   rightr
-                    
-                    if let Expr::Unary(_, rightrl) = *leftr {
-                        // !   p
-                        // _   rightrl
-                        leftr = rightrl;
-                        unary = true;
-                    }
+                if let Expr::Unary(_, rightrl) = *leftr {
+                    // !   p
+                    // _   rightrl
+                    leftr = rightrl;
+                    unary = true;
+                }
 
-                    if left != leftr {
-                        return;
-                    } else if (operator != Token::And && operator != Token::Or) || (operatorr != Token::Or && operatorr != Token::And) {
-                        return;
-                    } else if operator == operatorr {
-                        return;
-                    }
+                if left != leftr {
+                    return original;
+                } else if (operator != Token::And && operator != Token::Or) || (operatorr != Token::Or && operatorr != Token::And) {
+                    return original;
+                } else if operator == operatorr {
+                    return original;
+                }
 
-                    if unary {
-                        self.simplified = Expr::binary(*left, operator, *rightr);
-                    } else {
-                        self.simplified = *left;
-                    }
+                if unary {
+                    return Expr::binary(*left, operator, *rightr);
+                } else {
+                    return *left;
                 }
             }
-            _ => println!("NOT BINARY"),
+            return original;
         }
+        Expr::Grouping(e) => Expr::grouping(absorbtion(*e)),
+        _ => original
     }
 }
